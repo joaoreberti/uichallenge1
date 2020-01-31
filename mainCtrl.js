3bd5f246986fd0709286cfaddd0804848df326c6
reberti <jreberti@leads4sales.pt>
2020-01-31 17:57:54 +0000

    add edge cases

--- mainCtrl.js
+++ mainCtrl.js
@@ -4,27 +4,37 @@
 
 const mostFrequent = (lista) => {
 let newArray = [];
+let maisFrequent = null;
+let contadorMaisAlto= null;
+
     for(let i = 0; i < lista.length; i++) {
-       let maisFrequent = null;
         let currentNumber = lista[i];
         let contador = 0;
         for (let j = 0; j< lista.length;j++ ){
             if(lista[j] === currentNumber){
                 contador ++;
-                
-                
-            }
-             
             
-
+            }
+            if(contadorMaisAlto === null){
+                contadorMaisAlto = contador;
+            }else{
+                if(contador>contadorMaisAlto){
+                    contadorMaisAlto = contador;
+                    maisFrequent = lista[i];
+                }else{
+                }
+            }
+              
         }
-        newArray[i] = `${currentNumber} apareceu ${contador}`;
+        
         
 
-        console.log(newArray);
 
         
     }
+    console.log(maisFrequent);
+
 };
 
 mostFrequent([1,2,3,3]);
+mostFrequent([1,2,2,3,4,4]);
